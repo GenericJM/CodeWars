992279082cec722d6d37e4974eaa32e8e9350b67
GenericJam <juan.arturo.m.m@gmail.com>
2022-06-23 09:41:40 -0400

    add sortNumbers

--- toCamelCAse.js
+++ toCamelCAse.js
@@ -1,6 +1,6 @@
 "usestrict"
-const sentence="The-stealth-warrior";
+const str="The-stealth-warrior";
 const regex=/(-|_)[a-z]/g
-const match=sentence.replace(regex,(a)=>a[1].toUpperCase());
+const match=str.replace(regex,(a)=>a[1].toUpperCase());
 const match1=str.replace(/[-_](.)/g, (_, c) => c.toUpperCase());
 console.log(match," ",match1)  
